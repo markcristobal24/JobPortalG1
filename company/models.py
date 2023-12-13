7027cc67f829b2515309a43f37d352d619e05780
lancevicente <lancevicente50@gmail.com>
2023-12-13 23:21:29 +0800

    upload company logo

--- company/models.py
+++ company/models.py
@@ -8,6 +8,7 @@ class Company(models.Model):
     est_date = models.PositiveIntegerField(null = True, blank = True)
     city = models.CharField(max_length=100, null = True, blank = True)
     state = models.CharField(max_length=100, null = True, blank = True)
+    company_logo = models.FileField(upload_to="company", null=True, blank=True)
 
     def __str__(self):
         return self.name
